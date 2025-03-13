0dab213e59520a24ad2329eb522000315e55eda4
Nora Shaipi <shaipin@lafayette.edu>
2025-03-13 19:34:54 -0400

    Added first task: start a very simple server

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Define a route
+// define the route
 app.get('/', (req, res) => {
     res.send('Welcome to Datanz Facility API!');
 });
 
-// Start the server
+// start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
