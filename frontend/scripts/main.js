4e8f249e3ffec916510ccf23b145154d9a8cd8dc
Naomi Yilma <yilman@lafayette.edu>
2025-03-23 14:07:41 -0400

    updated main.js to randomly change colors

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,1 +1,8 @@
-//To handle main from index html
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("backgroundChanger");
+
+    button.addEventListener("click", function () {
+        const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+        document.body.style.backgroundColor = randomColor;
+    });
+});
