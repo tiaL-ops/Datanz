b1436e330afabb245843459919aaa2ef64407014
Landy Rakotoarison <landytiaa@gmail.com>
2025-05-02 18:25:03 -0400

    main updated

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('main');
+  const lang = req.query.lang || 'en';
+  res.render('main', { 
+    currentLang: lang 
+  });
 });
 
 module.exports = router;
